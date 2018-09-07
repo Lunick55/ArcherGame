c51d0cf28c6689f9d6b0b1fa03fdb3e697d9ddf5
Lunick <karimelzanaty98@gmail.com>
2018-09-06 21:22:12 -0400

    Added rotating bow that follows the mouse.

---
+++ Archer Test/Assets/Code/bowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowScript : MonoBehaviour {
+
+    Vector3 mousePos;
+    private float angle;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+
+        Rotate();
+	}
+
+    void Rotate()
+    {
+        Vector3 distance = (mousePos - gameObject.transform.position);
+
+        angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
